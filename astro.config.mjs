6cfa4c8a184b08277e67e0f2e489607637a3595b
André Miranda <aegis.andre@gmail.com>
2025-07-25 00:16:39 -0300

    :bug: fix: change base path for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import { defineConfig } from "astro/config"
 // https://astro.build/config
 export default defineConfig({
   site: "https://lucasbarrosal.github.io",
-  base: "turbofrio-web",
+  base: "/turbofrio-web",
+  build: {
+    assets: "assets",
+  },
 })
