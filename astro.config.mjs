cb357fd89a8bdbda32ddcb8257d3963a11e10363
André Miranda <aegis.andre@gmail.com>
2025-08-23 11:30:47 -0300

    :wrench: chore: change build config to default

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,4 @@
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://lucasbarrosal.github.io",
-  base: "/turbofrio-web",
-  build: {
-    assets: "assets",
-  },
-})
+export default defineConfig({})
